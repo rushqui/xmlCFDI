525fdd1af1659a7049f7213f9de38688d3299b99
elias_rojas <rushqui@gmail.com>
2019-07-29 14:16:04 -0500

    creating virtual enviroment and getting the order number fot the ordenCompra field in the addenda xml

--- adendaPdf.py
+++ adendaPdf.py
@@ -1,13 +1,16 @@
 import PyPDF2
 #save de pdf on a variable, second prameter is for reading
 #and open in binary mode
-ordenCompraPdf = open('Docs/Orden_de_Compra.pdf','rb')
+ordenCompraPdf = open('docs/Orden_de_Compra.pdf','rb')
 pdfReader = PyPDF2.PdfFileReader(ordenCompraPdf)
-
-print(pdfReader.numPages)
+#print(pdfReader.numPages)
 
 pageObj = pdfReader.getPage(0)
+factura_pdf_str = pageObj.extractText()
 
-print(pageObj.extractText())
+find_num_pedido = factura_pdf_str.find('Nº de pedido:') + 13
+final_num_pedido = find_num_pedido + 10
+#print(find_num_pedido)
+print(factura_pdf_str[find_num_pedido:final_num_pedido])
 
 
