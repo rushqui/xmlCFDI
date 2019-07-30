159d14b214a615056a7abd489a5dbcfb52d8d945
elias_rojas <rushqui@gmail.com>
2019-07-30 13:45:08 -0500

    extracting info from pdf and adding almost all fields of the addenda xml

--- adendaPdf.py
+++ adendaPdf.py
@@ -1,16 +1,46 @@
 import PyPDF2
+
 #save de pdf on a variable, second prameter is for reading
 #and open in binary mode
-ordenCompraPdf = open('docs/Orden_de_Compra.pdf','rb')
-pdfReader = PyPDF2.PdfFileReader(ordenCompraPdf)
-#print(pdfReader.numPages)
+def get_num_pedido():
 
-pageObj = pdfReader.getPage(0)
-factura_pdf_str = pageObj.extractText()
+    ordenCompraPdf = open('docs/Orden_de_Compra.pdf','rb')
+    pdfReader = PyPDF2.PdfFileReader(ordenCompraPdf)
+    #print(pdfReader.numPages)
 
-find_num_pedido = factura_pdf_str.find('Nº de pedido:') + 13
-final_num_pedido = find_num_pedido + 10
-#print(find_num_pedido)
-print(factura_pdf_str[find_num_pedido:final_num_pedido])
+    pageObj = pdfReader.getPage(0)
+    factura_pdf_str = pageObj.extractText()
+
+    #look for the index in the extracted text from the pdf
+    #plus the number of characters in the string No de pedido
+    find_num_pedido = factura_pdf_str.find('Nº de pedido:') + 13
+
+    #plus the characters size of the order number
+    final_num_pedido = find_num_pedido + 10
+
+    #get the substring for the order number
+    return factura_pdf_str[find_num_pedido:final_num_pedido]
+
+
+def get_num_proveedor():
+    
+    ordenCompraPdf = open('docs/Orden_de_Compra.pdf','rb')
+    pdfReader = PyPDF2.PdfFileReader(ordenCompraPdf)
+    #print(pdfReader.numPages)
+
+    pageObj = pdfReader.getPage(0)
+    factura_pdf_str = pageObj.extractText()
+
+    #look for the index in the extracted text from the pdf
+    #plus the number of characters in the string No de pedido
+    find_num_pedido = factura_pdf_str.find('CENTRO:') + 8
+
+    #plus the characters size of the order number
+    final_num_pedido = find_num_pedido + 5
+
+    #get the substring for the order number
+    return factura_pdf_str[find_num_pedido:final_num_pedido]
+
+
 
 
--- generaFacturaAdenda.py
+++ generaFacturaAdenda.py
@@ -1,4 +1,6 @@
 from xml.etree import ElementTree as ET
+from adendaPdf import get_num_pedido
+from adendaPdf import get_num_proveedor
 
 ET.register_namespace('mabe', "http://recepcionfe.mabempresa.com/cfd/addenda/v1")
 
@@ -17,8 +19,9 @@ mab_factura.set('tipoDocumento', 'FACTURA')
 mab_factura.set('folio', rootRead.get('Folio'))
 fecha = rootRead.get('Fecha').split('T')
 mab_factura.set('fecha',fecha[0])
-print(fecha[0])
-mab_factura.set('fecha','somefecha')
+num_orden_compra = get_num_pedido()
+mab_factura.set('ordenCompra', num_orden_compra)
+mab_factura.set('referencial','')
 
 
 mab_moneda = ET.SubElement(mab_factura,"{http://recepcionfe.mabempresa.com/cfd/addenda/v1}Moneda")
@@ -29,10 +32,11 @@ mab_proveedor = ET.SubElement(mab_factura, "{http://recepcionfe.mabempresa.com/c
 mab_proveedor.set('codigo', '')
 
 mab_entrega = ET.SubElement(mab_factura, "{http://recepcionfe.mabempresa.com/cfd/addenda/v1}Entrega")
-mab_entrega.set('plantaEntrega', '')
-mab_entrega.set('calle', '')
-mab_entrega.set('noExterior', '')
-mab_entrega.set('codigoPostal', '')
+planta_entrega = get_num_proveedor()
+mab_entrega.set('plantaEntrega', planta_entrega)
+mab_entrega.set('calle', 'INGENIEROS MILITARES')
+mab_entrega.set('noExterior', '156')
+mab_entrega.set('codigoPostal', '11210')
 
 mab_detalles = ET.SubElement(mab_factura, "{http://recepcionfe.mabempresa.com/cfd/addenda/v1}Detalles")
 
@@ -41,8 +45,9 @@ consecutive = 0
 for concepto in rootRead.iter('{http://www.sat.gob.mx/cfd/3}Concepto'):
     consecutive = consecutive + 1
     cantidad_str =  concepto.get('Cantidad')
-    unidad_str = concepto.get('Unidad')
+    cantidad_flt = float(cantidad_str)
     importe_str = concepto.get('Importe')
+    valor_unitario_str = concepto.get('ValorUnitario')
 
     importe_flt = float(importe_str)
 
@@ -51,31 +56,40 @@ for concepto in rootRead.iter('{http://www.sat.gob.mx/cfd/3}Concepto'):
     
     mab_detalle = ET.SubElement(mab_detalles, "{http://recepcionfe.mabempresa.com/cfd/addenda/v1}Detalle")
     mab_detalle.set('noLineaArticulo', str(consecutive))
-    mab_detalle.set('codigoArticulo', '')
+    mab_detalle.set('codigoArticulo', '0')
 
     descripcion_product = concepto.get('Descripcion')
     mab_detalle.set('descripcion', descripcion_product)
     
-    mab_detalle.set('unidad', unidad_str)
+    mab_detalle.set('unidad', 'UN')
     mab_detalle.set('cantidad', cantidad_str)
-    mab_detalle.set('precioSinIva', '')
-    mab_detalle.set('precioConIva', '')
-    mab_detalle.set('importeSinIva', '')
-    mab_detalle.set('importeConIva', '')
+    mab_detalle.set('precioSinIva', valor_unitario_str)
+    valor_unitario_flt = float(valor_unitario_str)
+    valor_unitario_iva = "{0:.2f}".format(valor_unitario_flt * 1.16)
+    mab_detalle.set('precioConIva', str(valor_unitario_iva))
+
+    importe_flt = valor_unitario_flt * cantidad_flt
+    importe_iva_flt = "{0:.2f}".format(importe_flt * 1.16)
+    mab_detalle.set('importeSinIva', str(importe_flt))
+    mab_detalle.set('importeConIva', str(importe_iva_flt))
 
 mab_subtotal = ET.SubElement(mab_factura, "{http://recepcionfe.mabempresa.com/cfd/addenda/v1}Subtotal")
-mab_subtotal.set('importe', '')
+subtotal = rootRead.get('SubTotal')
+mab_subtotal.set('importe', subtotal)
 
 mab_traslados = ET.SubElement(mab_factura, "{http://recepcionfe.mabempresa.com/cfd/addenda/v1}Traslados")
 mab_traslado = ET.SubElement(mab_traslados, "{http://recepcionfe.mabempresa.com/cfd/addenda/v1}Traslado")
-mab_traslado.set('tipo', '')
-mab_traslado.set('tasa', '')
-mab_traslado.set('importe', '')
+mab_traslado.set('tipo', 'IVA')
+mab_traslado.set('tasa', '0.16')
+get_traslados = rootRead.find('{http://www.sat.gob.mx/cfd/3}Impuestos')
+total_traslados = get_traslados.get('TotalImpuestosTrasladados')
+mab_traslado.set('importe', total_traslados)
 
 mab_retenciones = ET.SubElement(mab_factura, "{http://recepcionfe.mabempresa.com/cfd/addenda/v1}Retenciones")
 
 mab_total = ET.SubElement(mab_factura, "{http://recepcionfe.mabempresa.com/cfd/addenda/v1}Total")
-mab_total.set('importe', '')
+total = rootRead.get('Total')
+mab_total.set('importe', total)
 
 
 
