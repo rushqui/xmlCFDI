0d2491eb4cc87d9e06174d06ebb69580a7562bcb
elias_rojas <rushqui@gmail.com>
2019-08-21 12:30:53 -0500

    templates fr errors 500 and 404 are added

--- run.py
+++ run.py
@@ -34,6 +34,18 @@ db = SQLAlchemy(app)
 
 from models import User, Addenda, Proveedor
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html", error = error)
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template("500.html", error = error)
+
+@app.route('/error')
+def generate_server_error():
+    return 1/0
+    
 @app.route("/")
 def index():
     return render_template("index.html")
