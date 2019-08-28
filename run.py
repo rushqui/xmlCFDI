065834f7b1c0fb959e4d9906ca857031e7b673e5
elias_rojas <rushqui@gmail.com>
2019-08-27 23:38:49 -0500

    organizing templates and creating config script

--- run.py
+++ run.py
@@ -11,8 +11,9 @@ from forms import SignupForm, AddendaForm, LoginForm
 from adendaPdf import get_num_pedido, get_num_proveedor
 from mergeFacturas import _addenda_tag, _merge_facturas
 from common import wahio
+from app import create_app
 
-
+app = create_app()
 
 ALLOWED_EXTENSIONS = set(['pdf', 'xml'])
 DIRECCION_CALLE = 'PASEO DE LAS PALAMAS'
@@ -22,11 +23,6 @@ CODIGO_ARTICULO = '0'
 UNIDAD = 'UN'
 TIPO = 'IVA'
 TASA = '0.16'
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://monitor:password1@172.22.75.56:5432/monitordb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
